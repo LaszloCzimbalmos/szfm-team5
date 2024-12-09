8c6311dab50e9fdcc20593f805085a9e9b529aa6
Lacika <lacimaci2003@gmail.com>
2024-12-09 18:11:09 +0100

    fix
    fix backend

--- backend/src/main/java/coffee/backend/RegisterServlet.java
+++ backend/src/main/java/coffee/backend/RegisterServlet.java
@@ -6,16 +6,17 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "helloServlet", value = "/login")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "loginServlet", value = "/register")
+public class RegisterServlet extends HttpServlet {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/cofedb";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        String email = request.getParameter("email");
         String callerCode = request.getParameter("userid");
         String pawPrint = request.getParameter("pswrd");
 
@@ -27,17 +28,18 @@ public class HelloServlet extends HttpServlet {
             conn = DatabaseConnection.initializeDatabase();
 
             // Prepare the SQL statement to insert the user data
-            String sql = "INSERT INTO users (caller_code, paw_print) VALUES (?, ?)";
+            String sql = "INSERT INTO registered (email, caller_code, paw_print) VALUES (?, ?, ?)";
             stmt = conn.prepareStatement(sql);
-            stmt.setString(1, callerCode);
-            stmt.setString(2, pawPrint);
+            stmt.setString(1, email);
+            stmt.setString(2, callerCode);
+            stmt.setString(3, pawPrint);
 
             // Execute the statement
             int rowsInserted = stmt.executeUpdate();
 
             // Check if the user was successfully inserted
             if (rowsInserted > 0) {
-                response.sendRedirect("https://laszloczimbalmos.github.io/menu");
+                response.sendRedirect("https://laszloczimbalmos.github.io/");
             } else {
                 response.getWriter().write("Error adding user.");
             }
